4d2177417238653e42fe1493fb2beb31b320fcbb
Gabriel <contato.gabrieldefreitas@gmail.com>
2023-09-17 17:53:41 -0300

    feat: :sparkles: payment repository

--- src/main/java/com/unisinos/carrentsystem/repository/PaymentRepository.java
+++ src/main/java/com/unisinos/carrentsystem/repository/PaymentRepository.java
@@ -5,8 +5,8 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.unisinos.carrentsystem.entity.Book;
+import com.unisinos.carrentsystem.entity.Payment;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, UUID> {
+public interface PaymentRepository extends JpaRepository<Payment, UUID> {
 }
